8e01130458b4d449bf64736dddfe3483616a8998
Mo Afolayan <mo.afolayan@gmail.com>
2021-05-25 09:13:12 -0400

    adding function types

--- src/app/services/checklist/checklist.service.ts
+++ src/app/services/checklist/checklist.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { CheckListItem } from 'src/app/models/checklistitem';
 import { Recipe } from 'src/app/models/recipe';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,22 +14,22 @@ export class ChecklistService {
     private http: HttpClient
   ) { }
 
-  getActiveCheckList(recipeId: number) {
+  getActiveCheckList(recipeId: number): Observable<Object> {
     return this.http.get(environment['apiBaseUrl'] + '/checklist/' + recipeId);
   }
 
-  updateCheckListItem(checkListItem: CheckListItem) {
+  updateCheckListItem(checkListItem: CheckListItem): Observable<CheckListItem> {
     return this.http.post<CheckListItem>(environment['apiBaseUrl'] + '/checklist/updatechecklistitemstatus/', checkListItem);
   }
 
-  deactivateCheckList(checkListId: number) {
+  deactivateCheckList(checkListId: number): Observable<Object> {
     const data = {
       'id': checkListId
     };
     return this.http.post(environment['apiBaseUrl'] + '/checklist/deactivate', data);
   }
 
-  addNewCheckList(recipe: Recipe) {
+  addNewCheckList(recipe: Recipe): Observable<Object> {
     const data = {
       'recipeId': recipe.id,
     };
